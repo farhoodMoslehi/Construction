7a8e9a2cd478f5ccbe8a449a716a795a64c8ecb7
Farhood <farhood.moslehi@gmail.com>
2019-02-03 20:16:06 -0500

    the first code commit

---
+++ ConstructionProjects/DataAccessLayer/Datalayer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace DataAccessLayer
+{
+    class Datalayer
+    {
+        public string DBConnection()
+        {
+            return ConnectionString();
+        }
+        private string ConnectionString()
+        {
+            ConnectionStringSettings cs = ConfigurationManager.ConnectionStrings["Databasecon"];
+
+            if (cs != null)
+            {
+                return cs.ConnectionString;
+            }
+            else
+            {
+                string s = ConfigurationManager.ConnectionStrings["Databasecon"].ConnectionString;
+                return "something else";
+            }
+        }
+    }
+}
